6116d5e0b041a60c486410ddf1d1cdc82f4c9e56
Takuya Kitazawa <k.takuti@gmail.com>
2016-08-03 10:35:24 +0900

    simplify SDAR logic

--- changefinder.py
+++ changefinder.py
@@ -1,37 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-
-
-def LevinsonDurbin(r, lpcOrder):
-    """
-    from http://aidiary.hatenablog.com/entry/20120415/1334458954
-    """
-    a = np.zeros(lpcOrder + 1, dtype=np.float64)
-    e = np.zeros(lpcOrder + 1, dtype=np.float64)
-
-    a[0] = 1.0
-    a[1] = - r[1] / r[0]
-    e[1] = r[0] + r[1] * a[1]
-    lam = - r[1] / r[0]
-
-    for k in range(1, lpcOrder):
-        lam = 0.0
-        for j in range(k + 1):
-            lam -= a[j] * r[k + 1 - j]
-        lam /= e[k]
-
-        U = [1]
-        U.extend([a[i] for i in range(1, k + 1)])
-        U.append(0)
-
-        V = [0]
-        V.extend([a[i] for i in range(k, 0, -1)])
-        V.append(1)
-
-        a = np.array(U) + lam * np.array(V)
-        e[k + 1] = e[k] * (1.0 - lam * lam)
-
-    return a, e[-1]
+import numpy.linalg as ln
+from scipy.linalg import toeplitz
 
 
 class SDAR_1D:
@@ -64,26 +34,28 @@ class SDAR_1D:
             float: Logloss for x.
 
         """
-        assert xs.size >= self.order, 'xs must be order or more'
+        assert xs.size >= self.order, 'size of xs must be greater or equal to `order`'
 
         # estimate mu
         self.mu = (1 - self.r) * self.mu + self.r * x
 
-        # estimate c (Yule-Walker equation)
+        # update c (coefficients of the Yule-Walker equation)
         self.c[0] = (1 - self.r) * self.c[0] + self.r * (x - self.mu) * (x - self.mu)  # c_0: x_t = x_{t-j}
-        self.c[1:self.order] = (1 - self.r) * self.c[1:self.order] + self.r * (x - self.mu) * (xs[::-1][:(self.order - 1)] - self.mu)
+        self.c[1:] = (1 - self.r) * self.c[1:] + self.r * (x - self.mu) * (xs[::-1][:self.order] - self.mu)
 
         # solve the Yule-Walker equation
-        a, e = LevinsonDurbin(self.c, self.order)
+        # TODO: replace here with 1d computation
+        C = toeplitz(self.c[:self.order])
+        a = np.dot(ln.inv(C), self.c[1:])  # a_1, ..., a_k
 
         # estimate x
-        x_hat = np.dot(-a[1:], (xs[::-1] - self.mu)) + self.mu
+        x_hat = np.dot(a, (xs[::-1][:self.order] - self.mu)) + self.mu
 
         # estimate sigma
-        self.sigma = (1 - self.r) * self.sigma + self.r * (x - x_hat) * (x - x_hat)
+        self.sigma = (1 - self.r) * self.sigma + self.r * (x - x_hat) ** 2
 
         # compute the probability density function
-        p = np.exp(-0.5 * ((x - x_hat)**2) / self.sigma) / (np.sqrt(2 * np.pi) * np.sqrt(np.abs(self.sigma)))
+        p = np.exp(-0.5 * (x - x_hat) ** 2 / self.sigma) / ((2 * np.pi) ** (self.order / 2) * (self.sigma) ** 0.5)
 
         return -np.log(p)
 
