2194331c46b983c5b48bcc2f01ca840ccfeb01ff
Takuya Kitazawa <k.takuti@gmail.com>
2016-08-04 17:02:09 +0900

    add datadog API helper class

---
+++ datadog_api_helper.py
@@ -0,0 +1,46 @@
+from datadog import initialize, api
+
+
+class DatadogAPIHelper:
+
+    def __init__(self, app_key, api_key):
+        initialize(app_key=app_key, api_key=api_key)
+
+    def get_series(self, start, end, query):
+        """Get time series points.
+
+        Args:
+            start (int): Unix timestamp.
+            end (int): Unix timestamp.
+            query (string): Datadog query.
+
+        """
+        j = api.Metric.query(start=start, end=end, query=query)
+
+        if j['status'] != 'ok':
+            raise Exception('Status was NOT `ok`.')
+
+        series = []
+
+        for d in j['series']:
+            # p = [ timestamp, value ]
+            series += [{'scope': d['scope'],
+                        'time': int(p[0]),
+                        'raw_value': p[1]
+                        } for p in d['pointlist']]
+
+        return sorted(series, key=lambda d: d['time'])
+
+    def post_metric(self, metric, points, host):
+        """Post the given points to a specified metric with host information.
+
+        Args:
+            metric (str): Destination metric name.
+            points (one of belows):
+                p value
+                (p time, p value)
+                [(p_1 time, p_1 value), ..., (p_n time, p_n value)]
+            host: Metric source.
+
+        """
+        api.Metric.send(metric=metric, points=points, host=host)
