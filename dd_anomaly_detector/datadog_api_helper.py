32374199510e509f9619943164b3c331c74ed1f3
Takuya Kitazawa <k.takuti@gmail.com>
2016-08-24 11:16:45 +0900

    support slack notification for Datadog-API-related errors (#9)

--- dd_anomaly_detector/datadog_api_helper.py
+++ dd_anomaly_detector/datadog_api_helper.py
@@ -1,10 +1,44 @@
+import os
+import slackweb
+import configparser
+from functools import partial
 from datadog import initialize, api
+
+from logging import getLogger
+logger = getLogger('ChangeFinder')
 
 
 class DatadogAPIHelper:
 
-    def __init__(self, app_key, api_key):
+    def __init__(self, app_key, api_key, does_notify_slack=True):
         initialize(app_key=app_key, api_key=api_key)
+
+        self.does_notify_slack = does_notify_slack
+
+        # slack notification setting
+        if does_notify_slack:
+            self.load_slack_config()
+
+    def load_slack_config(self):
+        parser = configparser.ConfigParser()
+        parser.read(os.getcwd() + '/config/datadog.ini')
+
+        if 'slack' not in parser:
+            logger.warning('Datadog: Slack notification setting is true, but the configuration cannot be found from the .ini file.')
+            self.does_notify_slack = False
+            return
+
+        s = parser['slack']
+        self.slack = slackweb.Slack(url=s.get('url'))
+
+        channel = s.get('channel') or '#general'
+        username = s.get('username') or 'Bot'
+        icon_emoji = s.get('icon_emoji') or ':ghost:'
+
+        self.slack_notify = partial(self.slack.notify,
+                                    channel=channel,
+                                    username=username,
+                                    icon_emoji=icon_emoji)
 
     def get_series(self, start, end, query):
         """Get time series points.
@@ -18,9 +52,13 @@ class DatadogAPIHelper:
         j = api.Metric.query(start=start, end=end, query=query)
 
         if 'errors' in j:
-            raise RuntimeError('Datadog: %s' % j['errors'])
+            msg = 'Datadog: %s' % j['errors']
+            self.slack_notify(attachments=[{'text': msg, 'color': 'danger'}])
+            raise RuntimeError(msg)
         if 'status' in j and j['status'] != 'ok':
-            raise RuntimeError('Datadog: API status was NOT ok: %s' % j['status'])
+            msg = 'Datadog: API status was NOT ok: %s' % j['status']
+            self.slack_notify(attachments=[{'text': msg, 'color': 'danger'}])
+            raise RuntimeError(msg)
 
         series = []
 
