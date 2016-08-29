5f3f4d772f344be23fef7986e081db4dcc1029c4
Takuya Kitazawa <k.takuti@gmail.com>
2016-08-29 17:12:39 +0900

    #11: Implement additional model parameter estimation logics for AR(k)

--- core/changefinder/utils.py
+++ core/changefinder/utils.py
@@ -15,17 +15,6 @@ def aryule(c, k):
             Some formulations solve: C a = -c,
             but we actually solve C a = c.
 
-    TODO: Must be tested here
-    # recursively solve the Yule-Walker equation
-    if self.c[0] != 0:
-        for i in range(self.k):
-            a[i] = self.c[i + 1]
-
-            for j in range(i):
-                a[i] -= (a[j] * self.c[i - j])
-
-            a[i] /= self.c[0]
-
     """
     a = np.zeros(k)
 
@@ -35,3 +24,69 @@ def aryule(c, k):
         a = np.dot(ln.inv(C), c[1:])
 
     return a
+
+
+def aryule_levinson(c, k):
+    """MATLAB implementation of Levinson-Durbin recursion.
+
+    cf. https://searchcode.com/file/64213289/inst/levinson.m
+
+    """
+    if c[0] == 0:
+        return np.zeros(k)
+
+    # recursively solve the Yule-Walker equation
+    g = -c[1] / c[0]
+    a = np.array([g])
+    v = c[0] * (1 - g * g)
+
+    for t in range(1, k):
+        g = -(c[t + 1] + np.dot(a, c[1:(t + 1)][::-1])) / v
+        a = np.append(a + g * a[:t][::-1], g)
+        v = v * (1 - np.dot(g, g))
+
+    return -a
+
+
+def arburg(x, k):
+    """MATLAB implementation of the Burg's method.
+
+    cf. https://searchcode.com/codesearch/view/9503568/
+
+    """
+
+    def sumsq(x):
+        return np.sum(x * x)
+
+    n = x.size
+    # v = sumsq(x)
+
+    # f and b are the forward and backward error sequences
+    f = x[1:n]
+    b = x[:(n - 1)]
+
+    a = np.array([])
+
+    # remaining stages i=2 to p
+    for i in range(k):
+
+        # get the i-th reflection coefficient
+        denominator = sumsq(f) + sumsq(b)
+        g = 0 if denominator == 0 else 2 * np.sum(f * b) / denominator
+
+        # generate next filter order
+        if i == 0:
+            a = np.array([g])
+        else:
+            a = np.append(g, a - g * a[:(i)][::-1])
+
+        # keep track of the error
+        # v = v * (1 - g * g)
+
+        # update the prediction error sequences
+        old_f = np.empty_like(f)
+        old_f[:] = f
+        f = old_f[1:(n - i - 1)] - g * b[1:(n - i - 1)]
+        b = b[:(n - i - 2)] - g * old_f[:(n - i - 2)]
+
+    return -a[:k][::-1]
--- core/tests/test_changefinder.py
+++ core/tests/test_changefinder.py
@@ -7,30 +7,54 @@ import numpy as np
 from numpy.testing import assert_almost_equal
 
 try:
-    from core.changefinder.utils import aryule
+    from core.changefinder.utils import aryule, aryule_levinson, arburg
 except ImportError:
     sys.path.append(os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                  os.pardir), os.pardir))
-    from core.changefinder.utils import aryule
+    from core.changefinder.utils import aryule, aryule_levinson, arburg
 
 
-class ChangeFinderUtilsTest(TestCase):
+class ChangeFInderYuleWalkerTest(TestCase):
+
+    def setUp(self):
+        self.c1 = np.array([1, 2, 3])
+        self.a1 = np.array([1.33333333, 0.33333333])
+
+        self.c2 = np.array([143.85, 141.95, 141.45, 142.30, 140.60,
+                            140.00, 138.40, 137.10, 138.90, 139.85])
+        self.a2 = np.array([2.90380682, 0.31235631, 1.26463104,
+                            -3.30187384, -1.61653593, -2.10367317,
+                            1.37563117, 2.18139823, 0.02314717])
+
+        self.c3 = np.array([0, 0, 0])
+        self.a3 = np.array([0, 0])
 
     def test_aryule(self):
-        c = np.array([1, 2, 3])
-        k = 2
-        a = aryule(c, k)
-        assert_almost_equal(np.array([1.33333333, 0.33333333]), a)
+        assert_almost_equal(self.a1, aryule(self.c1, 2))
+        assert_almost_equal(self.a2, aryule(self.c2, 9))
+        assert_almost_equal(self.a3, aryule(self.c3, 2))
 
-        c = np.array([143.85, 141.95, 141.45, 142.30, 140.60,
-                      140.00, 138.40, 137.10, 138.90, 139.85])
-        k = 9
-        a = aryule(c, k)
-        assert_almost_equal(np.array([2.90380682, 0.31235631, 1.26463104,
-                                      -3.30187384, -1.61653593, -2.10367317,
-                                      1.37563117, 2.18139823, 0.02314717]), a)
+    def test_aryule_levinson(self):
+        assert_almost_equal(self.a1, aryule_levinson(self.c1, 2))
+        assert_almost_equal(self.a2, aryule_levinson(self.c2, 9))
+        assert_almost_equal(self.a3, aryule_levinson(self.c3, 2))
 
-        c = np.array([0, 0, 0])
-        k = 2
-        a = aryule(c, k)
-        assert_almost_equal(np.array([0, 0]), a)
+
+class ChangeFinderBurgTest(TestCase):
+
+    def setUp(self):
+        # Octave outputs
+        self.c1 = np.array([1, 2, 3, 4, 5])
+        self.a1 = np.array([-1.86391, 0.95710])
+
+        self.c2 = np.array([143.85, 141.95, 141.45, 142.30, 140.60,
+                            140.00, 138.40, 137.10, 138.90, 139.85])
+        self.a2 = np.array([-1.31033, 0.58569, -0.56058, 0.63859, -0.35334])
+
+        self.c3 = np.array([0, 0, 0, 0, 0])
+        self.a3 = np.array([0, 0])
+
+    def test_arburg(self):
+        assert_almost_equal(self.a1, arburg(self.c1, 2), decimal=5)
+        assert_almost_equal(self.a2, arburg(self.c2, 5), decimal=5)
+        assert_almost_equal(self.a3, arburg(self.c3, 2))
