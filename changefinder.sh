f01dcb71399b7b2b424edbd301a351394ad731aa
Takuya Kitazawa <k.takuti@gmail.com>
2016-09-30 11:18:20 +0900

    Fix travis run test config

--- changefinder.sh
+++ changefinder.sh
@@ -21,16 +21,16 @@ case "$1" in
   start)
     echo "Starting server"
     # Start the daemon
-    ~/.pyenv/shims/python /usr/share/datadog-anomaly-detector/daemonizer.py start ${CF_DAEMON_ARGS}
+    python /usr/share/datadog-anomaly-detector/daemonizer.py start ${CF_DAEMON_ARGS}
     ;;
   stop)
     echo "Stopping server"
     # Stop the daemon
-    ~/.pyenv/shims/python /usr/share/datadog-anomaly-detector/daemonizer.py stop ${CF_DAEMON_ARGS}
+    python /usr/share/datadog-anomaly-detector/daemonizer.py stop ${CF_DAEMON_ARGS}
     ;;
   restart)
     echo "Restarting server"
-    ~/.pyenv/shims/python /usr/share/datadog-anomaly-detector/daemonizer.py restart ${CF_DAEMON_ARGS}
+    python /usr/share/datadog-anomaly-detector/daemonizer.py restart ${CF_DAEMON_ARGS}
     ;;
   *)
     # Refuse to do other stuff
